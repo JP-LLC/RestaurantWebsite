f0c90b9b1155a1cd4c687b76e0e2c031b58c0301
Timothy Winfred <twinfred@gmail.com>
2018-07-18 15:48:25 -0700

    moved index gallery images to view from javascript

--- apps/orderFood/views.py
+++ apps/orderFood/views.py
@@ -5,4 +5,11 @@ from django.shortcuts import render, redirect, HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'orderFood/index.html')
+    context = {
+        'hero_images': [
+            'https://cdn.stocksnap.io/img-thumbs/960w/T65Y75HO94.jpg',
+            'https://cdn.stocksnap.io/img-thumbs/960w/KJGLHB2KF4.jpg',
+            'https://cdn.stocksnap.io/img-thumbs/960w/GL0X7W9QLC.jpg'
+        ]
+    }
+    return render(request, 'orderFood/index.html', context)
